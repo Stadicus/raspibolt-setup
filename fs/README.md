53bc0cd7b092857b11df28e627547bd2e2c62dc7
Stadicus <roland@stadler.org>
2018-08-02 23:07:20 +0200

    Initial PoC for DietPi automation (#1)
    * Create 1-firstboot.sh
    
    * Update and rename 1-firstboot.sh to trueno-1-firstboot.sh
    
    * Create trueno-2-install.sh
    
    * Update trueno-1-firstboot.sh
    
    * Update trueno-1-firstboot.sh
    
    * Update trueno-1-firstboot.sh
    
    * Create trueno-dietpi.sh
    
    * Update trueno-dietpi.sh
    
    * Update trueno-dietpi.sh
    
    * Rename scripts/trueno-dietpi.sh to trueno-dietpi.sh
    
    * Create bitcoin.conf
    
    * Create bitcoind.service
    
    * Create lnd.conf
    
    * Create lnd.service
    
    * Create 20-raspibolt-welcome
    
    * Update trueno-dietpi.sh
    
    * Create eps.service
    
    * Create ufw-config.sh
    
    * Rename trueno-dietpi.sh to scripts/trueno-dietpi.sh
    
    * Create .network
    
    * Rename fs/home/bitcoin/config/bitcoin.conf to config/bitcoin.conf
    
    * Create README.md
    
    * Create README.md
    
    * Create .gitignore
    
    * Update trueno-dietpi.sh
    
    * Update trueno-dietpi.sh
    
    * Update trueno-dietpi.sh
    
    * Delete trueno-1-firstboot.sh
    
    * Update trueno-dietpi.sh
    
    * Delete ufw-config.sh
    
    * Delete trueno-2-install.sh
    
    * Update and rename trueno-dietpi.sh to raspibolt-autoinit.sh
    
    * Rename fs/home/bitcoin/config/lnd.conf to config/lnd.conf
    
    * Rename fs/home/admin/.network to config/.network
    
    * DietPi configuration (copy to /boot)
    
    * Update .gitignore

---
+++ fs/README.md
@@ -0,0 +1,3 @@
+### /fs
+Files in the **fs** folder are core components and are copied directly to the file system. 
+They must not be edited by the user and are overwritten during an upgrade.
---
+++ scripts/raspibolt-autoinit.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+# RaspiBolt: init script for DietPi automation
+
+stateFile="/root/init.state"
+logFile="/var/log/trueno-init.log"
+
+echo "*** $(date +'%Y-%m/%d %H:%M:%S') - Trueno DietPi init start" >>$logFile 2>&1
+adduser admin --gecos "" --disabled-password >>$logFile 2>&1
+adduser admin sudo >>$logFile 2>&1
+adduser bitcoin --gecos "" --disabled-password >>$logFile 2>&1
+adduser web --gecos "" --disabled-password >>$logFile 2>&1
+
+apt-get install -y usbmount jq dphys-swapfile python python3 xxd ufw >>$logFile 2>&1
+
+if [ -d "/root/raspibolt-setup" ]; then
+  cd raspibolt-setup
+  git pull >>$logFile 2>&1
+  cd
+else
+  git clone https://github.com/Stadicus/raspibolt-setup >>$logFile 2>&1
+fi
+cp -R raspibolt-setup/fs/* / >>$logFile 2>&1
+
+if [ -f "$stateFile" ]; then
+  state=$( cat $stateFile )
+else
+  state=10
+  echo "$state" > $stateFile
+fi
+
+### 10: First step ###############################################################
+if [ $state = 10 ]; then
+  echo "*** init state: $state" >>$logFile 2>&1
+  state=20
+  echo "$state" > $stateFile
+fi
+
+### 20: Second step #############################################################
+if [ $state = 20 ]; then
+  echo "*** init state: $state" >>$logFile 2>&1
+  ufw default deny incoming
+  ufw default allow outgoing
+  ufw allow 22    comment 'allow SSH'
+  ufw allow 50002 comment 'allow Electrum'
+  ufw allow 9735  comment 'allow Lightning'
+  ufw allow 8333  comment 'allow Bitcoin mainnet'
+  ufw allow 18333 comment 'allow Bitcoin testnet'
+  echo "y" | ufw enable
+  systemctl enable ufw
+  
+  state=30
+  echo "$state" > $stateFile
+fi

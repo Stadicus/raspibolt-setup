0a965f899ad0d3f680a0e3a0d7110d7777d7404b
Stadicus <roland@stadler.org>
2018-08-02 22:37:11 +0200

    Update trueno-dietpi.sh

--- scripts/trueno-dietpi.sh
+++ scripts/trueno-dietpi.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-stateFile = "/root/init.state"
-logFile   = "/var/log/trueno-init.log"
+stateFile="/root/init.state"
+logFile="/var/log/trueno-init.log"
 
 echo "*** $(date +'%Y-%m/%d %H:%M:%S') - Trueno DietPi init start" >>$logFile 2>&1
-adduser admin          >>$logFile 2>&1
-adduser admin sudo     >>$logFile 2>&1
-adduser bitcoin        >>$logFile 2>&1
-adduser web            >>$logFile 2>&1
+adduser admin --gecos "" --disabled-password >>$logFile 2>&1
+adduser admin sudo >>$logFile 2>&1
+adduser bitcoin --gecos "" --disabled-password >>$logFile 2>&1
+adduser web --gecos "" --disabled-password >>$logFile 2>&1
+
 apt install -y usbmount jq dphys-swapfile python python3 xxd ufw >>$logFile 2>&1
 
 git clone https://github.com/Stadicus/raspibolt-setup >>$logFile 2>&1

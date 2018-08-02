c84d6f1cdd2095399fd5e6728c21952e6a753063
Stadicus <roland@stadler.org>
2018-08-02 21:53:40 +0200

    Update trueno-dietpi.sh

--- trueno-dietpi.sh
+++ trueno-dietpi.sh
@@ -6,7 +6,7 @@ adduser admin          >> /var/log/trueno-init.log
 adduser admin sudo     >> /var/log/trueno-init.log
 adduser bitcoin        >> /var/log/trueno-init.log
 adduser web            >> /var/log/trueno-init.log
-apt install -y jq dphys-swapfile python xxd ufw >> /var/log/trueno-init.log
+apt install -y jq dphys-swapfile python python3 xxd ufw >> /var/log/trueno-init.log
 
 git clone https://github.com/Stadicus/raspibolt-setup
 cp -R raspibolt-setup/* /
